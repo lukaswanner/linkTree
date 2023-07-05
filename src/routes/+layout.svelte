44d7c7f5753c64325da3ef42165b66bee623241b
Lukas � <lukas_wanner@outlook.de>
2023-07-05 14:33:15 +0200

    navbar somewhat done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+    import Navbar from "$lib/components/Navbar.svelte";
     import { user, userData } from "$lib/firebase";
     import "../app.css";
+    import type { LayoutServerData } from "./$types";
 
     // preload global data that we use in child components
     $user;
     $userData;
+    export let data: LayoutServerData;
 </script>
 
+<svelte:head>
+    <link rel="icon" href="https://fav.farm/🪶" />
+    <title>Plume</title>
+</svelte:head>
+
 <div class="min-h-screen flex flex-col">
+    <Navbar username={data.user?.username} />
     <slot />
 </div>
