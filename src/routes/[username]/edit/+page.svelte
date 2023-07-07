b590769e5fd7dc7a63d59de9c617e20cfb211d2c
Lukas🙏 <lukas.wanner@leicom.ch>
2023-07-07 17:28:22 +0200

    fixed profile styling a bit

--- src/routes/[username]/edit/+page.svelte
+++ src/routes/[username]/edit/+page.svelte
@@ -108,49 +108,32 @@
             @{$userData?.username}
         </h1>
 
-        <div class="group relative">
             <img
                 src={$userData.photoURL ?? "/user.png"}
                 alt="photoURL"
                 width="256"
                 class="mx-auto rounded-full"
             />
-            <a
-                on:click={() => void 0}
-                href="/{$userData.username}/edit/photo"
-                class="btn btn-sm btn-circle absolute right-9 top-2"
-            >
-                <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <g id="Edit / Edit_Pencil_02">
-                        <path
-                            id="Vector"
-                            d="M4 16.0001V20.0001L8 20.0001L18.8686 9.13146L18.8695 9.13061C19.265 8.73516 19.4628 8.53736 19.5369 8.3092C19.6021 8.10835 19.6022 7.89201 19.5369 7.69117C19.4627 7.46284 19.2646 7.26474 18.8686 6.86872L17.1288 5.12892C16.7345 4.7346 16.5369 4.53704 16.3091 4.46301C16.1082 4.39775 15.8919 4.39775 15.691 4.46301C15.463 4.53709 15.2652 4.73488 14.8704 5.12976L14.8686 5.13146L4 16.0001Z"
-                            stroke="currentColor"
-                            stroke-width="1"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                        />
-                    </g>
-                </svg>
-            </a>
-        </div>
 
         <div
             class="flex flex-row gap-4 items-center justify-center text-center my-4"
         >
             <a
-                class="btn btn-outline btn-xs"
+                class="btn btn-outline flex-1"
                 href={`/${$userData.username}/bio`}>Edit Bio</a
             >
+            <a
+                on:click={() => void 0}
+                href="/{$userData.username}/edit/photo"
+                class="btn btn-outline flex-1"
+            >
+            Edit Picture
+            </a>
         </div>
 
         <form class="form-control">
-            <label class="label cursor-pointer flex items-start justify-center">
-                <span class="label-text mr-6">
+            <label class="label cursor-pointer flex flex-row items-start gap-4">
+                <span class="label-text">
                     <div
                         class="tooltip group-hover:tooltip-open"
                         data-tip="If public, the world can see your profile"
