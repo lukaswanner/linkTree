021a48f9d595d7e42d5b2e7933dea57d046a6be0
Lukas � <lukas_wanner@outlook.de>
2023-07-09 22:21:28 +0200

    login route

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,7 @@
+<main
+    class="card md:w-3/6 w-5/6 bg-neutral text-neutral-content mx-auto my-auto"
+>
+    <div class="card-body items-center text-center">
+        <slot />
+    </div>
+</main>
