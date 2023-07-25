83e8ec30902d94535985ecae32653d7aafae05be
Lukas � <lukas_wanner@outlook.de>
2023-07-25 14:57:28 +0200

    changes

--- src/routes/(private)/+page.svelte
+++ src/routes/(private)/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import { userData } from "$lib/firebase";
+    import { onMount } from "svelte";
+    import { fly } from "svelte/transition";
+
+    let ready = false;
+    onMount(() => (ready = true));
 </script>
 
 <svelte:head>
@@ -7,39 +12,27 @@
     <meta name="description" content="one link to rule them all" />
 </svelte:head>
 
-<main class="flex w-full flex-1">
-    <div class="grid lg:grid-cols-2 gap-4 p-4">
-        <div class="w-full">
-            <h1 class="text-7xl font-bold leading-[6rem]">
-                Navigating the Digital Seas
-            </h1>
-            <p class="py-6 text-2xl">
-                Discover a world of possibilities with Lighthouse.
-            </p>
-            <div>
-                {#if $userData}
-                    <a href="/{$userData.username}/edit" class="btn btn-primary"
-                        >Edit Profile</a
-                    >
-                {:else}
-                    <div
-                        class="flex flex-row gap-2 justify-center items-center"
-                    >
-                        <a href="/signup" class="btn btn-primary">Get Started</a
-                        >
-                        <a href="/Lukas" class="btn btn-secondary"
-                            >Example Profile</a
-                        >
-                    </div>
-                {/if}
-            </div>
-        </div>
+<main class="w-full flex-1 flex flex-col gap-10 p-10">
+    <div class="w-full flex-col items-center text-center mt-10">
+        <h1 class=" text-7xl font-bold leading-[6rem]">
+            Navigating the Digital Seas
+        </h1>
+        <p class="py-6 text-2xl">
+            Discover a world of possibilities with Lighthouse.
+        </p>
         <div>
-            <div
-                class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-3xl"
-            >
-                <img src="/logo.png" alt="lighthouse" />
-            </div>
+            {#if $userData}
+                <a href="/{$userData.username}/edit" class="btn btn-primary"
+                    >Edit Profile</a
+                >
+            {:else}
+                <div class="flex flex-row gap-2 justify-center items-center">
+                    <a href="/signup" class="btn btn-primary">Get Started</a>
+                    <a href="/Lukas" class="btn btn-secondary"
+                        >Example Profile</a
+                    >
+                </div>
+            {/if}
         </div>
     </div>
 </main>
