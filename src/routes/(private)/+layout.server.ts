0a22b1c6b372766de671380aaed43fdc0c95d1a5
Lukas � <lukas_wanner@outlook.de>
2023-08-12 10:27:26 +0200

    removed attempt of auth solution

--- src/routes/(private)/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutServerLoad } from "./$types";
-// import { adminDB } from "$lib/server/admin";
-
-export const load = (async () => {
-    // const db = await adminDB.collection("users").get();
-
-    // Attach an asynchronous callback to read the data at our posts reference
-    return { };
-}) satisfies LayoutServerLoad;
--- src/routes/(private)/login/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ fetch }) => {
-    const res = await fetch("/api/auth", { method: "GET" });
-
-    console.log(res.status);
-    return {};
-};
